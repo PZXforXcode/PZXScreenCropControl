60c1002736783b2c0a1329b2639440501f672562
彭祖鑫 <496912046@qq.com>
2016-11-08 10:43:29 +0800

    Initial Commit

---
+++ PZXScreenCropControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PZXScreenCropControl
+//
+//  Created by pzx on 16/11/8.
+//  Copyright © 2016年 pzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PZXScreenCropControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PZXScreenCropControl
+//
+//  Created by pzx on 16/11/8.
+//  Copyright © 2016年 pzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
